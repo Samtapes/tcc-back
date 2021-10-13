8443c1402af1f4808dd4f6e5277c228346a5bc17
Gustavo Lima <samtapes48@gmail.com>
2021-10-13 12:36:04 -0300

    to heroku changes

---
+++ dist/knexfile.js
@@ -0,0 +1,49 @@
+"use strict";
+// Update with your config settings.
+exports.__esModule = true;
+exports.configuration = void 0;
+var configuration = {
+    development: {
+        client: 'sqlite3',
+        connection: {
+            filename: './src/database/db.sqlite'
+        },
+        migrations: {
+            directory: './src/database/migrations',
+            extension: 'ts'
+        },
+        useNullAsDefault: true
+    },
+    staging: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
+    },
+    production: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
+    }
+};
+exports.configuration = configuration;
+// module.exports = configuration;
